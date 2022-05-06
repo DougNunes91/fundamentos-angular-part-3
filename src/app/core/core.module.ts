80790d7c60994e6ff8c7743310484a6ba42a5453
Douglas <douglas.vnunes91@gmail.com>
2022-05-05 22:07:52 -0300

    Implementacao do logout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [HeaderComponent],
+  imports: [CommonModule],
   exports: [HeaderComponent]
 })
 export class CoreModule {}
